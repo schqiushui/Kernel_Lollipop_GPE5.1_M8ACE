fdcf15f1c6c28cd4c2fb25c8902cb71a6372e2c2
Shawn Guo <shawn.guo@linaro.org>
2012-07-13 08:19:34 +0100

    ARM: 7466/1: disable interrupt before spinning endlessly
    commit 98bd8b96b26db3399a48202318dca4aaa2515355 upstream.
    
    The CPU will endlessly spin at the end of machine_halt and
    machine_restart calls.  However, this will lead to a soft lockup
    warning after about 20 seconds, if CONFIG_LOCKUP_DETECTOR is enabled,
    as system timer is still alive.
    
    Disable interrupt before going to spin endlessly, so that the lockup
    warning will never be seen.
    
    Reported-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -281,6 +281,7 @@ void machine_shutdown(void)
 void machine_halt(void)
 {
 	machine_shutdown();
+	local_irq_disable();
 	while (1);
 }
 
@@ -307,6 +308,7 @@ void machine_restart(char *cmd)
 
 	
 	printk("Reboot failed -- System halted\n");
+	local_irq_disable();
 	while (1);
 }
 
