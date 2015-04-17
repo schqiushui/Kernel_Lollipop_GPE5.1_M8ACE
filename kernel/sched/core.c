14adc48b12f677a9e547c020aac321e8e5951839
Ruchi Kandoi <kandoiruchi@google.com>
2015-04-17 16:33:29 -0700

    sched: cpufreq: Adds a field cpu_power in the task_struct
    cpu_power has been added to keep track of amount of power each task is
    consuming. cpu_power is updated whenever stime and utime are updated for
    a task. power is computed by taking into account the frequency at which
    the current core was running and the current for cpu actively
    running at hat frequency.
    
    Bug: 21498425
    Change-Id: Ic535941e7b339aab5cae9081a34049daeb44b248
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -36,6 +36,7 @@
 #include <linux/interrupt.h>
 #include <linux/capability.h>
 #include <linux/completion.h>
+#include <linux/cpufreq.h>
 #include <linux/kernel_stat.h>
 #include <linux/debug_locks.h>
 #include <linux/perf_event.h>
@@ -2045,6 +2046,9 @@ void account_user_time(struct task_struct *p, cputime_t cputime,
 
 	
 	acct_update_integrals(p);
+
+	/* Account power usage for user time */
+	acct_update_power(p, cputime);
 }
 
 static void account_guest_time(struct task_struct *p, cputime_t cputime,
@@ -2082,6 +2086,9 @@ void __account_system_time(struct task_struct *p, cputime_t cputime,
 
 	
 	acct_update_integrals(p);
+
+	/* Account power usage for system time */
+	acct_update_power(p, cputime);
 }
 
 void account_system_time(struct task_struct *p, int hardirq_offset,
